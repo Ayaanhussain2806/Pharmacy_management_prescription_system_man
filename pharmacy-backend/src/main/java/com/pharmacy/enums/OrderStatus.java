55bb53334576bd9990c996e01159ad0b9063368e
ayaan <hussainayaan351@gmail.com>
2026-05-05 14:27:51 +0530

    add enums for order status

---
+++ pharmacy-backend/src/main/java/com/pharmacy/enums/OrderStatus.java
@@ -0,0 +1,12 @@
+package com.pharmacy.enums;
+
+public enum OrderStatus {
+    VERIFICATION_PENDING,
+    APPROVED,
+    REJECTED,
+    PROCESSING,
+    OUT_FOR_DELIVERY,
+    PICKED_UP,
+    DELIVERED,
+    CANCELLED
+}
